1249aa47f4962e1e4ee5122bbba5005d97e63889
Patrick Mwangi <frashid274@gmail.com>
2025-12-11 23:05:55 +0300

    Update next.config.ts to allow images from additional sources including Unsplash, Cloudinary, AWS, Google User Content, and GitHub Avatars.

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,23 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
       },
     ],
   },
